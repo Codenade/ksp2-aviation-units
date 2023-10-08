58478376eed7036aec3f73963deb4330e3b05743
Codenade <amadeus.dorian04@gmail.com>
2023-10-08 23:10:51 +0200

    simplification and version update for KSP2 0.1.4.1

--- ksp2-aviation-units/AviationUnitsPlugin.cs
+++ ksp2-aviation-units/AviationUnitsPlugin.cs
@@ -1,30 +1,33 @@
 using BepInEx;
 using KSP.Game;
-using KSP.Logging;
 using KSP.Messages;
 using KSP.UI.Binding;
 using KSP.UI.Binding.Core;
-using System;
-using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using UnityEngine;
-using static KSP.UI.Binding.UIValue_ReadNumber_TextUnits;
+using UnityEngine.InputSystem;
 
 namespace Codenade.AviationUnits
 {
-	[BepInPlugin("1b1f2cc6-9f9b-4ad7-b70c-2dbedafce2e8", "ksp2-aviation-units", "0.1.0")]
+    [BepInPlugin("1b1f2cc6-9f9b-4ad7-b70c-2dbedafce2e8", "ksp2-aviation-units", "0.1.1")]
 	sealed class AviationUnitsPlugin : BaseUnityPlugin
 	{
-        public static readonly string NAME = "ksp2-aviation-units";
-        private static bool _initialized;
+        public static readonly string NAME = typeof(AviationUnitsPlugin).GetCustomAttribute<BepInPlugin>().Name;
+        public static readonly string GUID = typeof(AviationUnitsPlugin).GetCustomAttribute<BepInPlugin>().GUID;
+        private InputAction _toggleAction;
 
 		void Awake()
 		{
-			gameObject.tag = "Game Manager";
-            if (!_initialized)
-                new Task(CreateAfterStartup).Start();
+            _toggleAction = new InputAction(GUID + "/toggle", binding: "<Keyboard>/f10");
+            _toggleAction.AddCompositeBinding("OneModifier")
+                .With("Binding", "<Keyboard>/f10")
+                .With("Modifier", "<Keyboard>/ctrl");
+            _toggleAction.Enable();
+            _toggleAction.performed += ctx =>
+            {
+                enabled = !enabled;
+            };
+            enabled = false;
 		}
 
 		void OnEnable()
@@ -39,85 +42,79 @@ namespace Codenade.AviationUnits
 			    GameManager.Instance.Game.Messages.Unsubscribe<VesselChangedMessage>(OnVesselChanged);
         }
 
-        void OnDestroy()
-        {
-            GlobalLog.Log(LogFilter.UserMod, $"[{NAME}] Destroyed by {new StackTrace(0, true)}");
-        }
-
-        static async void CreateAfterStartup()
-        {
-            await Task.Delay(20000);
-            _initialized = true;
-            GlobalLog.Log(LogFilter.UserMod, $"[{NAME}] Created after startup");
-            new GameObject(NAME, typeof(AviationUnitsPlugin));
-        }
-
 		void OnVesselChanged(MessageCenterMessage msg)
 		{
             SetAviationUnits();
 		}
 
-        static T_Field GetPrivateField<T_Target, T_Field>(object obj, string fieldName)
+        static T_Field GetPrivateFieldValue<T_Target, T_Field>(object obj, string fieldName)
         {
             return (T_Field)typeof(T_Target).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance).GetValue(obj);
         }
 
-        static void SetPrivateField<T_Target>(object obj, string fieldName, object value)
+        static void SetPrivateFieldValue<T_Target>(object obj, string fieldName, object value)
         {
             typeof(T_Target).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance).SetValue(obj, value);
         }
 
         public void SetAviationUnits()
         {
-            GlobalLog.Log(LogFilter.UserMod, $"[{NAME}] setting aviation units...");
+            // Set feet as altitude unit
             var altitudeLabel = GameObject.Find(@"GameManager/Default Game Instance(Clone)/UI Manager(Clone)/Scaled Main Canvas/FlightHudRoot(Clone)/group_navball(Clone)/Container/GRP-ALT/Container/DataContainer/Items/Value").GetComponent<UIValue_ReadNumber_TextUnits>();
             {
-                var unitEntryArray = GetPrivateField<UIValue_ReadNumber_TextUnits, UnitEntry[]>(altitudeLabel, "unitEntryArray");
+                var unitEntryArray = GetPrivateFieldValue<UIValue_ReadNumber_TextUnits, UIValue_ReadNumber_TextUnits.UnitEntry[]>(altitudeLabel, "unitEntryArray");
                 unitEntryArray[0].unitMultiplier = 0.3208f;
                 unitEntryArray[0].unitValue = "ft";
                 unitEntryArray[0].numberTextFormat = "{0:0}";
                 unitEntryArray[0].dontTruncateValue = false;
                 unitEntryArray[0].switchThresholdMultiplier = 0f;
-                SetPrivateField<UIValue_ReadNumber_TextUnits>(altitudeLabel, "unitEntryArray", unitEntryArray);
+                SetPrivateFieldValue<UIValue_ReadNumber_TextUnits>(altitudeLabel, "unitEntryArray", unitEntryArray);
             }
+
+            // Change altitude tape to feet too and be more sensitive
             var altitudeTape = GameObject.Find(@"GameManager/Default Game Instance(Clone)/UI Manager(Clone)/Scaled Main Canvas/FlightHudRoot(Clone)/group_navball(Clone)/Container/GRP-ALT/Container/DialBorder/DialMask").GetComponent<UIValue_ReadNumberValueTape>();
-            SetPrivateField<UIValue_ReadNumberValueTape>(altitudeTape, "_offsetUnits", 3);
-            SetPrivateField<UIValue_ReadNumberValueTape>(altitudeTape, "_valueInterval", 100f);
-            SetPrivateField<UIValue_ReadNumberValueTape>(altitudeTape, "numberTextFormat", "{0:0}");
+            SetPrivateFieldValue<UIValue_ReadNumberValueTape>(altitudeTape, "_offsetUnits", 3);
+            SetPrivateFieldValue<UIValue_ReadNumberValueTape>(altitudeTape, "_valueInterval", 100f);
+            SetPrivateFieldValue<UIValue_ReadNumberValueTape>(altitudeTape, "numberTextFormat", "{0:0}");
             {
-                var unitEntryArray = GetPrivateField<UIValue_ReadNumberValueTape, UnitEntry[]>(altitudeTape, "unitEntryArray");
+                var unitEntryArray = GetPrivateFieldValue<UIValue_ReadNumberValueTape, UIValue_ReadNumber_TextUnits.UnitEntry[]>(altitudeTape, "unitEntryArray");
                 unitEntryArray[0].unitMultiplier = 3.2808f;
                 unitEntryArray[0].unitValue = "ft";
                 unitEntryArray[0].dontTruncateValue = false;
                 unitEntryArray[0].switchThresholdMultiplier = 0f;
-                SetPrivateField<UIValue_ReadNumberValueTape>(altitudeTape, "unitEntryArray", unitEntryArray);
+                SetPrivateFieldValue<UIValue_ReadNumberValueTape>(altitudeTape, "unitEntryArray", unitEntryArray);
             }
+
+            // Set knots as speed unit
             var velocityLabel = GameObject.Find(@"GameManager/Default Game Instance(Clone)/UI Manager(Clone)/Scaled Main Canvas/FlightHudRoot(Clone)/group_navball(Clone)/Container/GRP-VEL/Container/DataContainer/Items/Value").GetComponent<UIValue_ReadNumber_TextUnits>();
             {
-                var unitEntryArray = GetPrivateField<UIValue_ReadNumber_TextUnits, UnitEntry[]>(velocityLabel, "unitEntryArray");
+                var unitEntryArray = GetPrivateFieldValue<UIValue_ReadNumber_TextUnits, UIValue_ReadNumber_TextUnits.UnitEntry[]>(velocityLabel, "unitEntryArray");
                 unitEntryArray[0].unitMultiplier = 0.5144f;
                 unitEntryArray[0].unitValue = "kt";
                 unitEntryArray[0].dontTruncateValue = true;
                 unitEntryArray[0].switchThresholdMultiplier = 1f;
-                SetPrivateField<UIValue_ReadNumber_TextUnits>(velocityLabel, "unitEntryArray", unitEntryArray);
+                SetPrivateFieldValue<UIValue_ReadNumber_TextUnits>(velocityLabel, "unitEntryArray", unitEntryArray);
             }
+
+            // Change speed tape to knots too and be more sensitive
             var velocityTape = GameObject.Find(@"GameManager/Default Game Instance(Clone)/UI Manager(Clone)/Scaled Main Canvas/FlightHudRoot(Clone)/group_navball(Clone)/Container/GRP-VEL/Container/DialBorder/DialMask").GetComponent<UIValue_ReadNumberValueTape>();
-            SetPrivateField<UIValue_ReadNumberValueTape>(velocityTape, "_valueInterval", 5.1444f);
-            SetPrivateField<UIValue_ReadNumberValueTape>(velocityTape, "_offsetUnits", 3);
-            SetPrivateField<UIValue_ReadNumberValueTape>(velocityTape, "numberTextFormat", "{0:00}");
+            SetPrivateFieldValue<UIValue_ReadNumberValueTape>(velocityTape, "_valueInterval", 5.1444f);
+            SetPrivateFieldValue<UIValue_ReadNumberValueTape>(velocityTape, "_offsetUnits", 3);
+            SetPrivateFieldValue<UIValue_ReadNumberValueTape>(velocityTape, "numberTextFormat", "{0:00}");
             {
-                var unitEntryArray = GetPrivateField<UIValue_ReadNumberValueTape, UnitEntry[]>(velocityTape, "unitEntryArray");
+                var unitEntryArray = GetPrivateFieldValue<UIValue_ReadNumberValueTape, UIValue_ReadNumber_TextUnits.UnitEntry[]>(velocityTape, "unitEntryArray");
                 unitEntryArray[0].unitMultiplier = 0.5144f;
                 unitEntryArray[0].unitValue = "kt";
                 unitEntryArray[0].dontTruncateValue = false;
                 unitEntryArray[0].switchThresholdMultiplier = 1000f;
-                SetPrivateField<UIValue_ReadNumberValueTape>(velocityTape, "unitEntryArray", unitEntryArray);
+                SetPrivateFieldValue<UIValue_ReadNumberValueTape>(velocityTape, "unitEntryArray", unitEntryArray);
             }
+
+            // Make vertical speed indicator show feet/minute
             var arrowIndicator = GameObject.Find(@"GameManager/Default Game Instance(Clone)/UI Manager(Clone)/Scaled Main Canvas/FlightHudRoot(Clone)/widget_indicator_verticalspeed_horizontal_new(Clone)/Container/Bound UI - VertSpeed/velocity - arrow indicator/Binding - Arrow Position").GetComponent<UIValue_ReadNumber_Dial>();
             typeof(UIValueNumberBinder).GetProperty("NumberRange").GetSetMethod(true).Invoke(arrowIndicator, new object[] { new UIValueNumberRange(-5.08d, 5.08d) });
-            SetPrivateField<UIValueNumberBinder>(arrowIndicator, "valueMappedMin", -5.08d);
-            SetPrivateField<UIValueNumberBinder>(arrowIndicator, "valueMappedMax", 5.08d);
-            GlobalLog.Log(LogFilter.UserMod, $"[{NAME}] setting aviation units finished");
+            SetPrivateFieldValue<UIValueNumberBinder>(arrowIndicator, "valueMappedMin", -5.08d);
+            SetPrivateFieldValue<UIValueNumberBinder>(arrowIndicator, "valueMappedMax", 5.08d);
         }
     }
 }
